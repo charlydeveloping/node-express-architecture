9f93ae36f4a7dfcd2824ff01dd1e8aafc33b60fa
Carlos Ramirez <cramirez@miteleferico.bo>
2021-11-16 21:57:26 -0400

    Auth changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,25 +1,33 @@
-const AuthService = require('../../services/auth.services')
-const UserService = require('../../services/user.services')
-const asyncHandler = require('../middlewares/async')
+const AuthService = require("../../services/auth.services");
+const UserService = require("../../services/user.services");
+const asyncHandler = require("../middlewares/async");
 
 const login = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  const token = await AuthService.login(email, password)
+  const token = await AuthService.login(email, password);
   res.status(200).json({
-    token
-  })
-})
+    token,
+  });
+});
+
+const logout = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "ok",
+  });
+});
 
 const me = asyncHandler(async (req, res) => {
-  const { uuid } = req.user
-  const user = await UserService.getOneUserByUUID(uuid)
+  const { uuid } = req.user;
+  const user = await UserService.getOneUserByUUID(uuid);
   res.status(200).json({
-    user
-  })
-})
+    user,
+  });
+});
 
 module.exports = {
   login,
-  me
-}
+  logout,
+  me,
+};
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,10 +1,11 @@
-const { Router } = require('express')
-const { validateJwt } = require('../middlewares/validate-jwt.middleware')
+const { Router } = require("express");
+const { validateJwt } = require("../middlewares/validate-jwt.middleware");
 
-const router = Router()
-const { login, me } = require('../controllers/auth.controller')
+const router = Router();
+const { login, me, logout } = require("../controllers/auth.controller");
 
-router.get('/me', validateJwt, me)
-router.post('/login', login)
+router.get("/me", validateJwt, me);
+router.post("/login", login);
+router.post("/logout", logout);
 
-module.exports = router
+module.exports = router;
